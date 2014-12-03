daa51f3387b0e67f8d81b0d0238b172bc7230aaa
Felix <satuday@gmail.com>
2014-12-03 02:09:56 -0500

    a

--- Game.cs
+++ Game.cs
@@ -7,24 +7,52 @@ namespace TicTacToa
 {
     public class Game
     {
-
+        Board board = new Board();
+        Player p1 = new Player();
     }
 
     public class Player
     {
-        public 
+        public List<int> Moves;
+        public Player()
+        {
+            Moves = new List<int>();
+        }
+
+        public void NextMove(int index)
+        {
+            Moves.Add(index);
+        }
     }
 
     public class Board
     {
-        List<int> availableBox;
-        List<Tuple<int, int, int>> winners;
-        
+        public List<int> availableBox;
+        List<Tuple<int, int, int>> winners;        
         
         public Board()
         {
-            
-            
+            initBoard();   
+        }
+
+        public void SetNextMove(int index)
+        {
+            availableBox.Remove(index);
+        }
+
+        public List<int> GetAvailableBox()
+        {
+            return availableBox;
+        }
+
+        public bool IsWinner(int[] boxes)
+        {
+            foreach (var t in winners)
+            {
+                if (boxes.Contains(t.Item1) && boxes.Contains(t.Item2) && boxes.Contains(t.Item3))
+                    return true;
+            }
+            return false;
         }
 
         void initBoard()
@@ -47,14 +75,10 @@ namespace TicTacToa
 
             winners.Add(new Tuple<int, int, int>(1, 4, 7));
             winners.Add(new Tuple<int, int, int>(2, 5, 8));
-            winners.Add(new Tuple<int, int, int>(3, 6, 3));
+            winners.Add(new Tuple<int, int, int>(3, 6, 9));
 
             winners.Add(new Tuple<int, int, int>(1, 5, 9));       
             winners.Add(new Tuple<int, int, int>(3, 5, 7));
-
-
         }
-
     }
-
 }
